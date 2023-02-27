eaf24ea1fccc02e95a5821947b5f300e09b2093a
OleksandrGnatiuk <oleksandr.gnatiuk@gmail.com>
2023-02-27 10:33:42 +0200

    changes func 'create_list'

--- task_2.py
+++ task_2.py
@@ -3,8 +3,8 @@ from multiprocessing import cpu_count, Pool, current_process
 import time
 from logger_ import get_logger
 
-
 logger = get_logger(__name__)
+
 
 def get_time(func):
     """Декоратор для визначення часу виконання функції"""
@@ -14,7 +14,7 @@ def get_time(func):
         start = time.time()
         result = func(*args, **kwargs)
         finish = time.time()
-        print(f"time: {finish-start}")
+        print(f"time: {finish - start}")
         return result
 
     return wrapper
@@ -36,17 +36,15 @@ def factorize(*number):
 
 # використовуємо multiprocessing:
 def create_list(num):
-    lst = []
-    for n in range(1, num + 1):
-        if num % n == 0:
-            lst.append(n)
+    lst = [n for n in range(1, num + 1) if num % n == 0]
     name = current_process().name
     logger.info(f"[{name}]: {num}")
     return lst
 
+
 @get_time
 def factorize_1(*number):
-    with Pool(cpu_count() * 2 + 1) as p:
+    with Pool(cpu_count() * 3) as p:
         result = p.map(create_list, number)
     return result
 
@@ -54,32 +52,8 @@ def factorize_1(*number):
 if __name__ == "__main__":
     a, b, c, d = factorize(128, 255, 99999, 10651060)
     a1, b1, c1, d1 = factorize_1(128, 255, 99999, 10651060)
-    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
-    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
-    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-    assert d == [
-        1,
-        2,
-        4,
-        5,
-        7,
-        10,
-        14,
-        20,
-        28,
-        35,
-        70,
-        140,
-        76079,
-        152158,
-        304316,
-        380395,
-        532553,
-        760790,
-        1065106,
-        1521580,
-        2130212,
-        2662765,
-        5325530,
-        10651060,
-    ]
+    assert a1 == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b1 == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c1 == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d1 == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395,
+                  532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]

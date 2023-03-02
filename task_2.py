0a4cefde1b42716d43193af64275d5eaa5cc6ef3
OleksandrGnatiuk <oleksandr.gnatiuk@gmail.com>
2023-03-02 14:38:24 +0200

    added ProcessPoolExecutor

--- task_2.py
+++ task_2.py
@@ -2,6 +2,7 @@ from functools import wraps
 from multiprocessing import cpu_count, Pool, current_process
 import time
 from logger_ import get_logger
+from concurrent.futures import ProcessPoolExecutor
 
 logger = get_logger(__name__)
 
@@ -48,12 +49,20 @@ def factorize_1(*number):
         result = p.map(create_list, number)
     return result
 
+@get_time
+def factorize_2(*number):
+    with ProcessPoolExecutor(cpu_count()) as executor:
+        result = executor.map(create_list, number)
+    return result
 
 if __name__ == "__main__":
     a, b, c, d = factorize(128, 255, 99999, 10651060)
     a1, b1, c1, d1 = factorize_1(128, 255, 99999, 10651060)
-    assert a1 == [1, 2, 4, 8, 16, 32, 64, 128]
-    assert b1 == [1, 3, 5, 15, 17, 51, 85, 255]
-    assert c1 == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
-    assert d1 == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395,
+    a2, b2, c2, d2 = factorize_2(128, 255, 99999, 10651060)
+
+    assert a2 == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b2 == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c2 == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d2 == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395,
                   532553, 760790, 1065106, 1521580, 2130212, 2662765, 5325530, 10651060]
+
